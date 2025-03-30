3fc038159d0903d8b5d88a1b0120db4745b40687
sahan <gunathungawsd.21@uom.lk>
2025-03-31 01:11:38 +0530

    Authentication Completed with Firebase

--- app/provider.js
+++ app/provider.js
@@ -1,8 +1,19 @@
 "use client"  
-import React from "react"
+import React, { useEffect } from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import {onAuthStateChanged} from 'firebase/auth'
 
 function Provider({children}) {
+  // This is a client component
+  
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      console.log(user);
+    });
+  
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div>
         <NextThemesProvider
