8e66123414768d931aff481daf25e8d4c3221819
sahan <gunathungawsd.21@uom.lk>
2025-04-01 22:56:18 +0530

    Authentication completed

--- app/provider.js
+++ app/provider.js
@@ -1,31 +1,43 @@
 "use client"  
-import React, { useEffect } from "react"
+import React, { useEffect,useState } from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import {onAuthStateChanged} from 'firebase/auth'
+import { auth } from "@/configs/firebaseConfig"
+import {AuthContext} from '../_context/AuthContext'
+import { useContext } from "react"
 
 function Provider({children}) {
   // This is a client component
-  
+
+  const[user,setUser]=useState();
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       console.log(user);
-    });
+      setUser(user);
+    },[]);
   
     return () => unsubscribe();
   }, []);
 
   return (
     <div>
-        <NextThemesProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-         >
-            {children}
-        </NextThemesProvider>
+      <AuthContext.Provider value={{user}}>
+          <NextThemesProvider
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem
+              disableTransitionOnChange
+          >
+              {children}
+          </NextThemesProvider>
+        </AuthContext.Provider>
     </div>
   )
+}
+export const useAuthContext=()=>{
+  const context=useContext(AuthContext);
+  return context
 }
 
 export default Provider
